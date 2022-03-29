5a977e4a294b227eac17da70098dea4d7da06ae5
Moemen Zaafarany <moemenzaafarany@gmail.com>
2022-03-29 10:22:01 +0200

    Update EbXhr.dart

--- EbTools/EbXhr.dart
+++ EbTools/EbXhr.dart
@@ -123,3 +123,60 @@ class EbXhrReponse {
     }
   }
 }
+
+//================================================
+class ExabyteAPIResponse {
+  // api response
+  final EbXhrReponse _res;
+  int? status;
+  String? statusCode;
+  String? error;
+  String? message;
+  dynamic data;
+  // parse data
+  ExabyteAPIResponse(this._res) {
+    if (_res.httpCode == 200 && _res.bodyJson != null) {
+      status = _res.bodyJson!['status'];
+      statusCode = _res.bodyJson!['statusCode'];
+      switch (_res.bodyJson!['status']) {
+        case 200:
+          message = _res.bodyJson!['message'];
+          data = _res.bodyJson!['data'];
+          break;
+        case 400:
+          error = _res.bodyJson!['error'];
+          data = _res.bodyJson!['data'];
+          break;
+        default:
+          error = _res.bodyJson!['error'];
+          break;
+      }
+    } else {
+      status = _res.httpCode;
+      error = _res.bodyText;
+    }
+  }
+  // run
+  run({
+    Function? s200,
+    Function? s400,
+    Function? s401,
+    Function? s403,
+    Function? s404,
+    Function? s500,
+    Function? sdefault,
+  }) {
+    if (status == 200 && s200 != null) return s200();
+    if (status == 400 && s400 != null) return s400();
+    if (status == 401 && s401 != null) return s401();
+    if (status == 403 && s403 != null) return s403();
+    if (status == 404 && s404 != null) return s404();
+    if (status == 500 && s500 != null) return s500();
+    if (sdefault != null) return sdefault();
+  }
+
+  /*
+  200 => "OK", 400 => "Bad Request", 401 => "Unauthorized", 403 => "Forbidden", 404 => "Not Found", 500 => "Internal Server Error"
+  */
+}
+
